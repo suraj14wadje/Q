71e4709229a253480d11a7c62bb5a5eab2d44c81
suraj wadje <suraj@litmusblox.io>
2021-05-04 16:59:13 +0530

    setup #2

--- setup/db.js
+++ setup/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const config   = require("config");
 
-
-mongoose.connect(config.get("db"),{ useUnifiedTopology: true ,useNewUrlParser: true })
-    .then(()=> log.info("db connected !"))
+module.exports = async()=>{
+    log.debug("connecting to database....")
+    await mongoose.connect(config.get("db"),{ useUnifiedTopology: true ,useNewUrlParser: true });
+    log.info("db connected !")
+}
